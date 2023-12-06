b6027642821de2c552fc969d134a1055faf4c61b
rgyarko <Rachameyawameyaw@gmail.com>
2023-12-05 21:23:51 -0500

    updating js

--- script.js
+++ script.js
@@ -7,7 +7,37 @@ var selectSymbol;
 var selectNumber;
 var selectUppercase;
 var selectLowercase;
+var lowercaseChars = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
 
+  var uppercaseChars = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+
+  var numberChars = "0,1,2,3,4,5,6,7,8,9";
+  
+  var symbolChars = "!,@,#,$,%,^,&,*,(),_,+,[],{},|,;,:,.,<>,?";
+function userOption(){
+  var passwordLength = prompt("Enter the length of the password:");
+
+  // Validate the user input for password length
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Please enter a valid password length between 8 and 128.");
+    return "";
+  }
+  selectLowercase=confirm("would you like to incloud lowercase character?;")
+  selectNumber=confirm("would you like include number in your password?;")
+  selectUppercase=confirm("would you like include uppercase in your password?;")
+  selectSymbol=confirm("would you like include symbol in your password?;")
+
+  var choices={
+    passwordLength:passwordLength,
+    selectLowercase:selectLowercase,
+    selectUppercase:selectUppercase,
+    selectSymbol:selectSymbol,
+    selectNumber:selectNumber,
+
+  }
+  return choices;
+
+}
 //  #password input
 function writePassword() {
   var password = generatePassword();
@@ -16,29 +46,26 @@ function writePassword() {
 // Function to generate a random password
 function generatePassword() {
   // Define character sets based on user input
-  var lowercaseChars = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
-
-  var uppercaseChars = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
-
-  var numberChars = "0,1,2,3,4,5,6,7,8,9";
-  
-  var symbolChars = "!,@,#,$,%,^,&,*,(),_,+,[],{},|,;,:,.,<>,?";
-
+ var option= userOption()
+var lowerArray= Array.from(lowercaseChars);
+var upperArray= Array.from(uppercaseChars);
+var numberArray= Array.from(numberChars);
+var symbolArray= Array.from(symbolChars);
   // Initialize an empty string to store the characters for the password
-  var allChars = "";
+  var allChars = [];
 
   // Concatenate character sets based on user input
-  if (selectLowercase) {
-    allChars += lowercaseChars;
+  if (option.selectLowercase) {
+    allChars =allChars.concat(lowerArray);
   }
-  if (selectUppercase) {
-    allChars += uppercaseChars;
+  if (option.selectUppercase) {
+    allChars =allChars.concat(upperArray);
   }
-  if (selectNumber) {
-    allChars += numberChars;
+  if (option.selectNumber) {
+    allChars.concat(numberArray);
   }
-  if (selectSymbol) {
-    allChars += symbolChars;
+  if (option.selectSymbol) {
+    allChars.concat(symbolArray);
   }
 
   // Check if the user has selected at least one character set
@@ -48,13 +75,7 @@ function generatePassword() {
   }
 
   // Get the length of the password from the user
-  var passwordLength = prompt("Enter the length of the password:");
-
-  // Validate the user input for password length
-  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-    alert("Please enter a valid password length between 8 and 128.");
-    return "";
-  }
+  
 
   // Generate the password
   var generatedPassword = "";
