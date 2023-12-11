810d4707c6d80f0e8fbfd3f53cd9bfdd1dd95225
Rgyarko <142129533+Rgyarko@users.noreply.github.com>
2023-12-11 18:25:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -119,7 +119,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+}
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
